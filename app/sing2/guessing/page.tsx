2a6ecb034a2b9081ebfa5dacb70b7cbb7d755612
kangjin lee <kangjinlee@kangjins-MacBook-Pro.local>
2025-10-10 14:50:36 +0900

    Fix TypeScript error in sort function

--- app/sing2/guessing/page.tsx
+++ app/sing2/guessing/page.tsx
@@ -736,7 +736,7 @@ export default function GuessingPage() {
 
                   {/* A, B, C 버튼들 - JSON의 label 사용, A, B, C 순서로 정렬 */}
                   {currentQuestion.options
-                    .sort((a, b) => a.label.localeCompare(b.label))
+                    .sort((a: any, b: any) => a.label.localeCompare(b.label))
                     .map((option, index) => (
                     <button
                       key={option.label}
